3edb677c7606008f5ce2a4582fa96e6f3b11453e
Charlie McClung <charlie@bowtie.co>
2022-04-24 21:33:07 -0600

    Rebuild and more art, restrict to curl

--- src/public/sh/slideshow.sh
+++ src/public/sh/slideshow.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Library of random txt art
 array[0]="200/color/elliot-hello-friend.txt"
 array[1]="200/color/hello-friend-tall.txt"
 array[2]="200/color/hello-friend.txt"
@@ -8,6 +9,21 @@ array[4]="derp-60.txt"
 array[5]="derp-100.txt"
 array[6]="glhf.txt"
 array[7]="welcome.txt"
+array[8]="dbz.txt"
+array[9]="elliot-hackerman.txt"
+array[10]="ftw.txt"
+array[11]="hack.txt"
+array[12]="hackerman.txt"
+array[13]="hackermansq.txt"
+array[14]="haha.txt"
+array[15]="hahas.txt"
+array[16]="lol.txt"
+array[16]="lols.txt"
+array[18]="mantra.txt"
+array[19]="nginx-full.txt"
+array[20]="nginx-icon.txt"
+array[21]="nginx-logo.txt"
+array[22]="nginx.txt"
 
 warn() {
         yellow='\033[0;33m'
