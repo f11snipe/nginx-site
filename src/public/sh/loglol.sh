b98218248c919794b646fe252a6f3471084f3970
F11snipe <f11snipe@gmail.com>
2022-04-29 18:31:37 -0600

    Update latest loglol script

--- src/public/sh/loglol.sh
+++ src/public/sh/loglol.sh
@@ -2,8 +2,10 @@
 
 log_name=${1:-access}.log
 log_dir=/var/log/nginx
+#out_dir=/tmp/logs
 out_dir=/var/www/f11snipe.sh/logs
 log_file=$log_dir/$log_name
 out_file=$out_dir/$log_name
 
-tail -f $log_file | sed -r 's/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b/wtf.lol.gud.try/g' > $out_file
+tail -f $log_file | stdbuf -oL -eL sed -r 's/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b/wtf.lol.gud.try/g' >> $out_file
+
